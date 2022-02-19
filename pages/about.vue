635da02126d12527526c9cb1691cd28063ad0618
Jonas Lörken <hi@iamjonas.de>
2022-02-19 01:35:32 +0100

    Überarbeitung

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Über mich</h1>
+    <p>Dies bin ich.</p>
+    <NuxtLink to="/">Startseite</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Startseite</h1>
+    <p>Dies ist die Startseite.</p>
+    <NuxtLink to="/about">Über mich</NuxtLink>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  transition: 'index'
 }
 </script>
+
+<style>
+  .index-enter-active, .index-leave-active { transition: all .5s; }
+  .index-enter, .index-leave-active { opacity: 0; transform: translateY(20px) }
+</style>
